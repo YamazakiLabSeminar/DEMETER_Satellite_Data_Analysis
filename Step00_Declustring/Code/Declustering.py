d12943b2f6c4005abcf3321f9d6ad668e5e4411f
Nan-zhiyuan <csch21058@g.nihon-u.ac.jp>
2025-09-19 15:24:22 +0900

    Implement code changes to enhance functionality and improve performance

---
+++ Step00_Declustring/Code/Declustering.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+df = pd.read_csv(r'C:\Users\nzy27\Documents\Github\DEMETER_Satellite_Data_Analysis\Step00_Declustring\Data\Earthquake_catalog\Original\EarthquakeCatalog_Depth40kmbelow_Magnitude4.8above.csv', encoding='cp932')
+time_list = pd.to_datetime(df['time'])
+output_df = pd.DataFrame({'year':time_list.dt.year,
+                          'month':time_list.dt.month,
+                          'day':time_list.dt.day,
+                          'hour':time_list.dt.hour,
+                          'minute':time_list.dt.minute,
+                          'second':time_list.dt.second,
+                          'microsecond':time_list.dt.microsecond,
+                          'latitude':df['latitude'],
+                          'longitude':df['longitude'],
+                          'magnitude':df['mag'],
+                          'depth':df['depth']
+                          })
+print(output_df)
+output_df.to_csv(r'C:\Users\nzy27\Documents\Github\DEMETER_Satellite_Data_Analysis\Step00_Declustring\Data\Earthquake_catalog\Declustering\EarthquakeCatalog_Depth40kmbelow_Magnitude4.8above_Declutering.csv', index=False, encoding='utf-8-sig')
