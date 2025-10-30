3db324047bcf3e516c1e425ed0787ece02918a8f
Nan-zhiyuan <csch21058@g.nihon-u.ac.jp>
2025-10-30 09:50:34 +0900

    Refactor code structure for improved readability and maintainability

--- Step00_Declustring/Code/Data_Pretreatment.py
+++ Step00_Declustring/Code/Data_Pretreatment.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv(r'C:\Users\nzy27\Documents\Github\DEMETER_Satellite_Data_Analysis\Step00_Declustring\Data\Earthquake_catalog\Original\EarthquakeCatalog_Depth40kmbelow_Magnitude4.8above.csv', encoding='cp932')
+DATA = r'C:\Users\nzy27\Documents\Github\DEMETER_Satellite_Data_Analysis\Step00_Declustring\Data\eq_m4.8above_depth40kmbelow_200407-201012.csv'
+OUTPUT_FILE = r'C:\Users\nzy27\Documents\Github\DEMETER_Satellite_Data_Analysis\Step00_Declustring\Output\eq_m4.8above_depth40kmbelow_200407-201012_add_time_row.csv'
+df = pd.read_csv(DATA, encoding='utf-8-sig')
 time_list = pd.to_datetime(df['time'])
 output_df = pd.DataFrame({'year':time_list.dt.year,
                           'month':time_list.dt.month,
@@ -14,5 +16,5 @@ output_df = pd.DataFrame({'year':time_list.dt.year,
                           'magnitude':df['mag'],
                           'depth':df['depth']
                           })
-print(output_df)
-output_df.to_csv(r'C:\Users\nzy27\Documents\Github\DEMETER_Satellite_Data_Analysis\Step00_Declustring\Data\Earthquake_catalog\Time_seperate\EarthquakeCatalog_Depth40kmbelow_Magnitude4.8above_TimeSeperate.csv', index=False, encoding='utf-8-sig')
+
+output_df.to_csv(OUTPUT_FILE, index=False, encoding='utf-8-sig')
