e64213b3475cf8a221a42f043eafbc7cdbd6a6d7
Nan-zhiyuan <csch21058@g.nihon-u.ac.jp>
2025-11-18 12:57:25 +0900

    Enhance cumulative comparison plots function with sorting and cumulative count; add output image

--- Step00_Declustring/Code/Cumulative_Comparison.py
+++ Step00_Declustring/Code/Cumulative_Comparison.py
@@ -1,19 +1,53 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+original_eq_file=r'C:\Users\nzy27\Documents\Github\DEMETER_Satellite_Data_Analysis\Step00_Declustring\Output\eq_m4.8above_depth40kmbelow_200407-201012_add_time_row.csv'
+declustring_eq_file=r'C:\Users\nzy27\Documents\Github\DEMETER_Satellite_Data_Analysis\Step00_Declustring\Output\all_eq_declustring_30day_30km.csv'
+save_path=r'C:\Users\nzy27\Documents\Github\DEMETER_Satellite_Data_Analysis\Step00_Declustring\graphic\cumulative_cimparison_plots_30days_30km.png'
+
 def cumulative_comparison_plots(
     original_eq_file, declustring_eq_file,
-    save_path=None    
+    save_path    
 ):
 
-# データの読み取り
-df_org = pd.read_csv(original_eq_file)
-df_declus = pd.csv(declustring_eq_file)
+    # データの読み取り
+    df_org = pd.read_csv(original_eq_file)
+    df_declus = pd.read_csv(declustring_eq_file)
 
-# 時間をdatetime形に変形
-df_org['datetime'] = pd.to_datetime(df_org['year', 'month', 'day', 'hour', 'minute', 'second'])
-df_declus['datetime'] = pd.to_datetime(df_declus['year', 'month', 'day', 'hour', 'minute', 'second'])
+    # 時間をdatetime形に変形
+    df_org['datetime'] = pd.to_datetime(df_org[['year', 'month', 'day', 'hour', 'minute', 'second']])
+    df_declus['datetime'] = pd.to_datetime(df_declus[['year', 'month', 'day', 'hour', 'minute', 'second']])
 
-# 
+    # 時系列昇順にソートする
+    df_org = df_org.sort_values('datetime')
+    df_declus = df_declus.sort_values('datetime')
+
+    # 一応確認
+    print(f'[Info]This is sorted dataframe.Original:{df_org} Declustring:{df_declus}')
+
+    # 両dfに累積カウントの列を追加する
+    df_org['cumulative_count'] = range(1, len(df_org)+1)
+    df_declus['cumulative_count'] = range(1, len(df_declus)+1)
+
+    # プロット
+    plt.figure(figsize=(12, 6))
+    plt.plot(df_org['datetime'], df_org['cumulative_count'], label='Original (all earthquakes)', color='red', linewidth=1.5)
+    plt.plot(df_declus['datetime'], df_declus['cumulative_count'], label='Declustring (all earthquakes)', color='blue', linewidth=1.5)
+
+    plt.xlabel('Date')
+    plt.ylabel('Cumulative Number (M4.8 above, Depth 40km below )')
+    plt.title('Comparison of Cumulative Earthquake Counts\nBefore and After Declustring(30km-30days)')
+    plt.legend()
+    plt.grid(True, alpha=0.3)
+    plt.tight_layout()
+
+    # 保存
+    plt.savefig(save_path, dpi=300)
+    plt.show()
+
 
 if __name__ == "__main__":
+    cumulative_comparison_plots(
+    original_eq_file, declustring_eq_file,
+    save_path    
+)
