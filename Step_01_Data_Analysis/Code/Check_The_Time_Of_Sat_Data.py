5e25550143c599da09fddd278ac2c3cbe33f87c4
Nan-zhiyuan <csch21058@g.nihon-u.ac.jp>
2025-11-19 19:07:03 +0900

    nothing

--- Step_01_Data_Analysis/Code/Check_The_Time_Of_Sat_Data.py
+++ Step_01_Data_Analysis/Code/Check_The_Time_Of_Sat_Data.py
@@ -9,12 +9,3 @@ current_dir = os.chdir(SSD_DIRECTORY)
 
 print(f'[Info] 現在のディレクトリ:{os.getcwd()}')
 
-for time_dir in current_dir:
-
-    if os.path.exists('F:/02_Selected_by_Time'):
-        continue
-    else:
-        time_dir = os.mkdir(r'F:/02_Selected_by_Time')
-
-current_dir = os.chdir(time_dir)
-print(f'[Info] 現在のdir:{os.getcwd()}')
