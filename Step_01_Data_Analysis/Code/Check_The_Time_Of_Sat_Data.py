c3304a10cf37e535824bb6c99cf9238cc6bb0023
Nan-zhiyuan <csch21058@g.nihon-u.ac.jp>
2025-11-19 19:06:23 +0900

    many fixes

--- Step_01_Data_Analysis/Code/Check_The_Time_Of_Sat_Data.py
+++ Step_01_Data_Analysis/Code/Check_The_Time_Of_Sat_Data.py
@@ -2,5 +2,19 @@ import pandas as pd
 import numpy as np
 import os
 
-dic=os.getcwd()
-print(f'dic:{dic}')
+SSD_DIRECTORY = r'F:/'
+Sat_Data_Dic = r'F:/01_EFdata'
+
+current_dir = os.chdir(SSD_DIRECTORY)
+
+print(f'[Info] 現在のディレクトリ:{os.getcwd()}')
+
+for time_dir in current_dir:
+
+    if os.path.exists('F:/02_Selected_by_Time'):
+        continue
+    else:
+        time_dir = os.mkdir(r'F:/02_Selected_by_Time')
+
+current_dir = os.chdir(time_dir)
+print(f'[Info] 現在のdir:{os.getcwd()}')
