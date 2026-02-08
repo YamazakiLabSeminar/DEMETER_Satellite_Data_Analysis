6afa20cde921d8b12e2346b65a5792b56d35de77
Nan-zhiyuan <csch21058@g.nihon-u.ac.jp>
2026-02-08 11:41:35 +0900

    １

--- src/step3_orbit_eq_match/check_distance.py
+++ src/step3_orbit_eq_match/check_distance.py
@@ -8,7 +8,7 @@
 # Explanation of each object:
 # -------------------------------------------------------------------------------------------------
 #[1.    Importing Modules]
-# 1-1.  Importing pandas, pathlib, and tqdm.auto
+# 1-1.  Importing pandas, pathlib, tqdm.auto, math.
 #
 #
 #[2.  Setting the directory path]
@@ -50,9 +50,94 @@
 # 6-17.     Calculating several parameters which are necessary to caculate the distance.
 # 6-18.     Calculating the distance between the epicenter and the orbit in km.
 # 6-19.     Cheking whether the distance meeting distance requirement.
-# 6-20.     
+# 6-20.     Inserting the data into a list object.
+#
+#
+# [7.       Exproting the data as a csv file]
+# 7-1.      Converting a list object into a data frame object.
+# 7-2.      Defining the columns of data frame.
+# 7-3.      Exporting the data frame object as a csv file.
+#
+#
 #**************************************************************************************************
 # Structure of this script:
 #--------------------------------------------------------------------------------------------------
-#
+import pandas as pd
+from pathlib import Path
+from tqdm.auto import tqdm
+import math
+
+ORBIT_DATA_DIR  = Path(r"E:\interim\step2_normalized")
+OB_MEET_TIME    = Path(r"E:\tables\orbit_meet_time_condition.csv")
+OUTPUT_DIR      = Path(r"E:\tables")
+
+print("orbit data directory =", {ORBIT_DATA_DIR})
+print("Orbit_meet_time_condion.csv path=", {OB_MEET_TIME})
+print("Directory of output", {OUTPUT_DIR})
+
+a = 6378137                                                         # the long radius of the Earth
+b = 6356752.314                                                     # the short radius of the Earth
+
+e = math.sqrt((a*a - b*b)/(a*a))                                    # eccentricity of the Earth
+
+df_obmt = pd.read_csv(OB_MEET_TIME)
+length_omt = len(df_obmt)
+
+df_obmt["4hour_before"] = pd.to_datetime(df_obmt["4hour_before"], format="mixed", utc=True).dt.tz_localize(None)
+df_obmt["datetime"] = pd.to_datetime(df_obmt["datetime"], format="mixed", utc=True).dt.tz_localize(None)
+df_obmt["eq_lat_rad"] = df_obmt["eq_lat"] * math.pi / 180
+df_obmt["eq_lon_rad"] = df_obmt["eq_lon"] * math.pi / 180
+
+list1 = [[] for k in range(length_omt)]
+
+for i in tqdm(range(length_omt), desc="Searching", unit="eq"):
+    list2 = []
+    orbit_file_name = df_obmt["orbit_file"].iloc[i]
+    
+    orbit_data_path = ORBIT_DATA_DIR / orbit_file_name
+
+    if not orbit_data_path.exists():
+        print(f"Missing orbit data file: {orbit_data_path}")
+        raise SystemExit(1)
+
+    df_obdata = pd.read_csv(orbit_data_path)
+    df_obdata["lat_rad"] = df_obdata["lat"] * math.pi / 180
+    df_obdata["lon_rad"] = df_obdata["lon"] * math.pi / 180
+    df_obdata["datetime"] = pd.to_datetime(df_obdata["datetime"]).dt.tz_localize(None)
+
+    lat1 = df_obmt["eq_lat_rad"].iloc[i]
+    lon1 = df_obmt["eq_lon_rad"].iloc[i]
+    beforeq = df_obmt["4hour_before"].iloc[i]
+    starteq = df_obmt["datetime"].iloc[i]
+
+    for j in range(len(df_obdata)):
+        lat2 = df_obdata["lat_rad"].iloc[j]
+        lon2 = df_obdata["lon_rad"].iloc[j]
+
+        dif_lat = lat2 - lat1
+        dif_lon = lon2 - lon1
+        if dif_lon > math.pi:
+            dif_lon -= 2*math.pi
+        elif dif_lon < -math.pi:
+            dif_lon += 2*math.pi
+        
+        P = (lat1+lat2) / 2                                         # 両点緯度の平均値
+        W = math.sqrt((1-e*e * math.sin(P) * math.sin(P)))
+        M = (a*(1 - e*e)) / (W * W * W)
+        N = a / W
+
+        dist = math.sqrt(dif_lat*dif_lat*M*M + dif_lon*dif_lon*N*N*math.cos(P)*math.cos(P))
+        dist = dist / 1000                                          # 単位[m] => [km]
+
+        if dist < 330:
+            s1 = df_obdata["datetime"].iloc[j]
+            if beforeq <= s1 <= starteq:
+                list2.append(orbit_file_name)
+                break
+    
+    list1[i] = list2
+
+data = pd.DataFrame(list1, columns=["orbit_meet_time_dist"])
+output_one = pd.concat([data, df_obmt], axis=1)
+output_one.to_csv(OUTPUT_DIR/"orbit_quake_distance_ver1.csv", index=False)
 #**************************************************************************************************
--- src/step3_orbit_eq_match/search_orbit.py
+++ src/step3_orbit_eq_match/search_orbit.py
@@ -35,26 +35,25 @@ searchdata = pd.read_csv(ORBIT_PATH)
 #
 #3-2.  Importing the earthquak data from a file
 eq_data = pd.read_csv(EQ_PATH)
-#
+eq_data["4hour_before"] = pd.to_datetime(eq_data["4hour_before"]).dt.strftime("%Y%m%d%H%M%S").astype(int)
+eq_data["datetime"]     = pd.to_datetime(eq_data["datetime"]).dt.strftime("%Y%m%d%H%M%S").astype(int)
+
 #
 #[4.  Preparation of searching]
 #4-1. Caculate the length of earthquake data
 length_eqdata = len(eq_data)
 #
 #4-2.  Making a list. This will contains orbit data which meet time requirement.
-list1 = []
+list1 = [[] for k in range(length_eqdata)]
 #
 #
 #[5.  Searching orbit which meet time requirement]
 for i in tqdm(range(length_eqdata), desc="EQ", unit="eq"):
-#5-1.  Setting time four hour before the earthquake occurred.
-    beforeq = pd.to_datetime(eq_data["4hour_before"].iloc[i])
-    starteq = pd.to_datetime(eq_data["datetime"].iloc[i])
-#
-#5-2.  Conversing the time into int format
-    beforeq = int(beforeq.strftime("%Y%m%d%H%M%S"))
-    starteq = int(starteq.strftime("%Y%m%d%H%M%S"))
-#
+    beforeq = eq_data["4hour_before"].iloc[i]
+    starteq = eq_data["datetime"].iloc[i]
+
+    list2 = []
+
 #5-3.  Searching orbits which meet time requiremnt.
     for j in range(len(searchdata)):
 #        
@@ -66,15 +65,17 @@ for i in tqdm(range(length_eqdata), desc="EQ", unit="eq"):
         if starteq < s1:
             break
         elif beforeq <= s1 <= starteq:
-            list1.append([searchdata.iloc[j, 0]] + eq_data.iloc[i, 0:7].tolist())
+            list2.append(searchdata.iloc[j,0])
         elif beforeq <= e1 <= starteq:
-            list1.append([searchdata.iloc[j, 0]] + eq_data.iloc[i, 0:7].tolist())
+            list2.append(searchdata.iloc[j,0])
+        
+    list1[i] = list2
 #
 #
 #[6.  Exporting data as a csv format file]
 #6-1.  converting a list object into a data frame object.
-data = pd.DataFrame(list1, columns=["orbit_file", "eq_id", "4hour_before", "datetime", "eq_lat", "eq_lon", "depth", "mag"])
-#
+data = pd.DataFrame(list1, columns=["orbit_meet_time_1","orbit_meet_time_2","orbit_meet_time_3"])
+outputone = pd.concat([eq_data, data], axis=1)
 #6-3.  Exporting the data frame into a csv file.
-data.to_csv(OUTPUT_DIR / "orbit_meet_time_condition.csv")
+outputone.to_csv(OUTPUT_DIR / "orbit_quake_ver1.csv", index=False)
 ###################################################################################################
